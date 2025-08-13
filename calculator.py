87e03a274597972755d09765ca83872659118c3c
feziiimt-rgb <feziiimt@gmail.com>
2025-08-13 11:02:54 +0530

    calculator program

--- calculator.py
+++ calculator.py
@@ -2,4 +2,5 @@
 a=10
 b=20
 print("addition of two number is :,a+b)
-print("subtraction of two number is :,a+b)
+print("subtraction of two number is :,a-b)
+print("multiplication of two number is :,a*b)
